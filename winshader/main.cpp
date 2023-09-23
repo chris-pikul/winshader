8b70002ddf750b7a4db959e5052131fb0dad0eb9
Chris Pikul <chris@workmaple.com>
2023-09-23 18:57:20 +0300

    starting point for WinRT/Win32

---
+++ winshader/main.cpp
@@ -0,0 +1,87 @@
+/**
+ * WinShader - Shader based windows fun.
+ * 
+ * Copyright 2023 Chris Pikul. All Rights Reserved.
+ * This code is licensed under the MIT License (MIT).
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::UI;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Composition::Desktop;
+
+// Forward declare the windows main function
+int CALLBACK WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int);
+
+// Forward declare the windows procedure handler function
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Implementation of windows main function. Program entry.
+int CALLBACK WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ LPSTR cmdLine, _In_ int cmdShow) {
+    // Initialize WinRT threading model for COM
+    init_apartment(apartment_type::single_threaded);
+
+    // Setup window class for program display
+    WNDCLASSEX wndClass = {};
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = instance;
+    wndClass.hIcon = LoadIcon(instance, MAKEINTRESOURCE(IDI_APPLICATION));
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = L"WinShader";
+    wndClass.hIconSm = LoadIcon(wndClass.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+    WINRT_VERIFY(RegisterClassEx(&wndClass));
+
+    // Build window instance itself
+    HWND hWnd = CreateWindow(
+        L"WinShader",
+        L"WinShader",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800,
+        600,
+        NULL,
+        NULL,
+        instance,
+        NULL
+    );
+    WINRT_VERIFY(hWnd);
+
+    ShowWindow(hWnd, cmdShow);
+    UpdateWindow(hWnd);
+
+    // Event/Message Loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+	return (int)msg.wParam;
+}
+
+// Implementation of windows procedure function. Handles event loop messages.
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    return 0;
+}
---
+++ winshader/pch.h
@@ -0,0 +1,28 @@
+// Precompiled headers for Win32 stuff
+#pragma once
+
+// Standard Library
+#include <memory>
+#include <atomic>
+#include <cmath>
+
+// Windows RT
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <winrt/Windows.UI.Popups.h>
+#include <winrt/Windows.Graphics.Capture.h>
+#include <winrt/Windows.Graphics.DirectX.h>
+#include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+
+// DirectX
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+#include <d2d1_3.h>
+#include <wincodec.h>
+
+// Miscellaneous
+#include <Unknwn.h>
+#include <inspectable.h>
